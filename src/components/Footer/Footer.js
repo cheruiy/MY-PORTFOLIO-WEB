d7d720f22839adee0eb21114e8e24f99a5446215
Cheruiyot Robert <kiruir38@gmail.com>
2025-02-24 15:27:46 +0300

    Initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/Thairu-dev/Task-Manager'
+      className='link footer__link'
+    >
+      Created By Cheruiyot Robert
+    </a>
+  </footer>
+)
+
+export default Footer
